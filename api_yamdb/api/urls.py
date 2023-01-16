06c860d2b8dc4a308f8fc930ff7e91c5bcf74f51
Александр Майстришин <aleksandrmajstrisin@MacBook-Pro-Aleksandr.local>
2023-01-16 22:17:57 +0300

    created applications reviews and API

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,4 @@
+from django.urls import include, path
+app_name = 'api_v1'
+urlpatterns = [
+]
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
