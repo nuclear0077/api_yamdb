74283b9969f283b1a10356b1fb58173ddd00902d
Александр Майстришин <nuclear0077@gmail.com>
2023-01-23 09:15:06 +0300

    вынес всю загрузку в отдельный класс

--- api_yamdb/api_yamdb/management/commands/load_data.py
+++ api_yamdb/api_yamdb/management/commands/load_data.py
@@ -8,71 +8,72 @@ import os
 from api.utils import email_is_valid
 
 #:TODO сделать временную директорию и туда выгружать подготовленные данные чтобы не портить исходящие данные
-class Command(BaseCommand):
-    dict_models = {
-        Genre: 'genre',
-        Category: 'category',
-        Title: 'titles',
-        TitleGenre: 'genre_title',
-        YamUser: 'users',
-        Review: 'review',
-        Comment: 'comments',
-    }
-    path_data = f'{settings.BASE_DIR}/static/data/'
-    files_dict = {
-        'category': f'{path_data}category.csv',
-        'comments': f'{path_data}comments.csv',
-        'genre_title': f'{path_data}genre_title.csv',
-        'genre': f'{path_data}genre.csv',
-        'review': f'{path_data}review.csv',
-        'titles': f'{path_data}titles.csv',
-        'users': f'{path_data}users.csv'
-    }
+class LoadData():
+    def __init__(self):
+        self.__dict_models = {
+            Genre: 'genre',
+            Category: 'category',
+            Title: 'titles',
+            TitleGenre: 'genre_title',
+            YamUser: 'users',
+            Review: 'review',
+            Comment: 'comments',
+        }
+        self.__path_data = f'{settings.BASE_DIR}/static/data/'
+        self.__files_dict = {
+            'category': f'{self.__path_data}category.csv',
+            'comments': f'{self.__path_data}comments.csv',
+            'genre_title': f'{self.__path_data}genre_title.csv',
+            'genre': f'{self.__path_data}genre.csv',
+            'review': f'{self.__path_data}review.csv',
+            'titles': f'{self.__path_data}titles.csv',
+            'users': f'{self.__path_data}users.csv'
+        }
 
-    def clean_data(self):
-        for model in self.dict_models.keys():
+    def __clean_data(self):
+        for model in self.__dict_models.keys():
             model.objects.all().delete()
         print('Все модели очищены')
 
-    def dir_is_exist(self, path):
+    def __dir_is_exist(self, path):
         if not os.path.exists(path):
             raise FileExistsError(f'Каталог data по пути {path} не найден')
         print('Все файлы находяться в каталоге')
 
-    def files_is_exist(self, path_files):
+    def __files_is_exist(self, path_files):
         for file in path_files:
             if not os.path.isfile(file):
                 raise FileExistsError(f'Файл по пути {file} не найден')
 
-    def prepare_data(self):
+    def __prepare_data(self):
         """Функция подготовки данных
             данная функция удаляет дубликаты и записи для которых нет записей
             в других таблица
             используя merge
         """
-        genre = pd.read_csv(self.files_dict.get('genre'))
+        genre = pd.read_csv(self.__files_dict.get('genre'))
         genre.drop_duplicates(['name'], keep='first', inplace=True)
         genre.drop_duplicates(['slug'], keep='first', inplace=True)
-        category = pd.read_csv(self.files_dict.get('category'))
+        category = pd.read_csv(self.__files_dict.get('category'))
         category.drop_duplicates(['name'], keep='first', inplace=True)
         category.drop_duplicates(['slug'], keep='first', inplace=True)
-        titles = pd.read_csv(self.files_dict.get('titles'))
+        titles = pd.read_csv(self.__files_dict.get('titles'))
         if 'description' not in titles.columns:
             titles.insert(loc=3, column='description', value=None)
         titles.rename(columns={'id': 'id_titles'}, inplace=True)
         titles_columns = titles.columns
         titles.drop_duplicates(['name'], keep='first', inplace=True)
-        genre_title = pd.read_csv(self.files_dict.get('genre_title'))
+        genre_title = pd.read_csv(self.__files_dict.get('genre_title'))
         genre_columns = genre_title.columns
         titles_genre = titles.merge(genre_title, how='inner', left_on='id_titles', right_on='title_id')
         titles = titles_genre[titles_columns].copy()
         titles.rename(columns={'id_titles': 'id'}, inplace=True)
         titles.drop_duplicates(['id'], keep='first', inplace=True)
-        titles.to_csv(self.files_dict.get('titles'), index=False)
-        category.to_csv(self.files_dict.get('category'), index=False)
-        titles_genre[genre_columns].to_csv(self.files_dict.get('genre_title'), index=False)
-        genre.to_csv(self.files_dict.get('genre'), index=False)
-        users = pd.read_csv(self.files_dict.get('users'))
+        titles.to_csv(self.__files_dict.get('titles'), index=False)
+        category.to_csv(self.__files_dict.get('category'), index=False)
+        titles_genre[genre_columns].to_csv(self.__files_dict.get('genre_title'), index=False)
+        genre.to_csv(self.__files_dict.get('genre'), index=False)
+        users = pd.read_csv(self.__files_dict.get('users'))
         users[['is_superuser', 'is_staff', 'is_active']] = None
         allowed_roles = ['admin', 'superuser', 'moderator', 'user']
         lst_idx_to_remove = []
@@ -98,8 +99,8 @@ class Command(BaseCommand):
         users.drop_duplicates(['id'], keep='first', inplace=True)
         users.drop_duplicates(['username'], keep='first', inplace=True)
         users.drop_duplicates(['email'], keep='first', inplace=True)
-        users.to_csv(self.files_dict.get('users'), index=False)
-        review = pd.read_csv(self.files_dict.get('review'))
+        users.to_csv(self.__files_dict.get('users'), index=False)
+        review = pd.read_csv(self.__files_dict.get('review'))
         review.drop_duplicates(['id'], keep='first', inplace=True)
         review.drop_duplicates(['title_id', 'author'], keep='first', inplace=True)
         review = review[(review['score'] >= 0) &  (review['score'] <= 10)]
@@ -114,8 +115,8 @@ class Command(BaseCommand):
         review_titles_merge.drop(columns=['id'], inplace=True)
         review_titles_merge.rename(columns={'id_review': 'id'}, inplace=True)
         review = review_titles_merge[review_columns]
-        review.to_csv(self.files_dict.get('review'), index=False)
-        comments = pd.read_csv(self.files_dict.get('comments'))
+        review.to_csv(self.__files_dict.get('review'), index=False)
+        comments = pd.read_csv(self.__files_dict.get('comments'))
         comments.drop_duplicates(['id'], keep='first', inplace=True)
         comments.rename(columns={'id': 'comments_id', 'text': 'comment_text', 'author': 'comment_author', 'pub_date': 'comment_pub_date'}, inplace=True)
         comments_columns = comments.columns
@@ -124,12 +125,12 @@ class Command(BaseCommand):
         comments_review_merge = comments.merge(review, how='inner', left_on='review_id', right_on='id')
         comments = comments_review_merge[comments_columns].copy()
         comments.rename(columns={'comments_id': 'id', 'comment_text': 'text', 'comment_author': 'author', 'comment_pub_date': 'pub_date'}, inplace=True)
-        comments.to_csv(self.files_dict.get('comments'), index=False)
+        comments.to_csv(self.__files_dict.get('comments'), index=False)
         print('Все данные подготовлены')
 
-    def load_data_users(self, model, key):
+    def __load_data_users(self, model, key):
         users_list = []
-        with open(self.files_dict.get(key), newline='') as csvfile:
+        with open(self.__files_dict.get(key), newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 users_list.append(model(
@@ -146,9 +147,9 @@ class Command(BaseCommand):
             model.objects.bulk_create(users_list)
         return True
 
-    def load_data_review(self, model, key):
+    def __load_data_review(self, model, key):
         users_list = []
-        with open(self.files_dict.get(key), newline='') as csvfile:
+        with open(self.__files_dict.get(key), newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 users_list.append(model(
@@ -171,23 +172,29 @@ class Command(BaseCommand):
                 ])
         return True
 
-    def load_data(self):
-        for model, key, in self.dict_models.items():
+    def __load_data(self):
+        for model, key, in self.__dict_models.items():
             if key == 'users':
-                self.load_data_users(model, key)
+                self.__load_data_users(model, key)
                 continue
             if key == 'review':
-                self.load_data_review(model, key)
+                self.__load_data_review(model, key)
                 continue
-            with open(self.files_dict.get(key), newline='') as csvfile:
+            with open(self.__files_dict.get(key), newline='') as csvfile:
                 reader = csv.reader(csvfile)
                 data = [model(*row) for idx, row in enumerate(reader) if idx != 0]
                 model.objects.bulk_create(data)
         print('Все данные успешно загружены в БД')
 
-    def handle(self, *args, **kwargs):
-        self.clean_data()
-        self.dir_is_exist(self.path_data)
-        self.files_is_exist(self.files_dict.values())
-        self.prepare_data()
-        self.load_data()
+    def run(self):
+        self.__clean_data()
+        self.__dir_is_exist(self.__path_data)
+        self.__files_is_exist(self.__files_dict.values())
+        self.__prepare_data()
+        self.__load_data()
+
+
+class Command(BaseCommand):
+    def handle(self):
+        load_data = LoadData()
+        load_data.run()
